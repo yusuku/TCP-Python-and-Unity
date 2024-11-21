7c8072c05370681c7556728d21217bc868c77d1a
yusuku <121999446+yusuku@users.noreply.github.com>
2024-11-21 15:39:36 +0900

    Add files via upload

--- TCP.cs
+++ TCP.cs
@@ -1,59 +1,122 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Net;
 using UnityEngine;
+using System.Threading;
+using UnityEngine.UI;
 
 public class TCP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private TcpListener Listener;
+    private Thread listenerThread;
+    private bool isRunning = true;
+    public RawImage rawImage;  
+    private Texture2D texture;
     void Start()
     {
-        ConnectPython();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void ConnectPython()
-    {
-        // エンドポイントを設定する
-        IPEndPoint RemoteEP = new IPEndPoint(IPAddress.Any, 50007);
-
-        // TcpListenerを作成する
-        TcpListener Listener = new TcpListener(RemoteEP);
-
-        // TCP接続を待ち受ける
+        // TCPリスナーを開始
+        Listener = new TcpListener(IPAddress.Any, 50007);
         Listener.Start();
-        TcpClient Client = Listener.AcceptTcpClient();
+        Debug.Log("TCP Listener started");
 
-        // 接続ができれば、データをやり取りするストリームを保存する
-        NetworkStream Stream = Client.GetStream();
-
-        GetPID(Stream);
-
-        // 接続を切る
-        Client.Close();
+        // リスナーを別スレッドで動作
+        listenerThread = new Thread(ListenForClients);
+        listenerThread.IsBackground = true;
+        listenerThread.Start();
     }
 
-
-    void GetPID(NetworkStream Stream)
+    void OnApplicationQuit()
     {
-        Byte[] data = new Byte[256];
-        String responseData = String.Empty;
+        // 終了時にリスナーとスレッドを停止
+        isRunning = false;
+        listenerThread?.Join(); // スレッド終了を待機
+        Listener?.Stop();
+        Debug.Log("TCP Listener stopped");
+    }
 
-        // 接続先からデータを読み込む
-        Int32 bytes = Stream.Read(data, 0, data.Length);
+    private void ListenForClients()
+    {
+        while (isRunning)
+        {
+            try
+            {
+                // クライアント接続を待機
+                if (Listener.Pending()) // クライアントが接続している場合のみ受け入れ
+                {
+                    using (TcpClient client = Listener.AcceptTcpClient())
+                    {
+                        Debug.Log("Client connected");
+                        HandleClient(client);
+                    }
+                }
+                else
+                {
+                    // クライアントが接続されるのを待つ
+                    Thread.Sleep(10); // CPU負荷を軽減
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Error: {e.Message}");
+                break;
+            }
+        }
+    }
 
-        // 読み込んだデータを文字列に変換する
-        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-        UnityEngine.Debug.Log("PID: " + responseData);
+    private void HandleClient(TcpClient client)
+    {
+        try
+        {
+            using (NetworkStream stream = client.GetStream())
+            {
+                while (isRunning)
+                {
+                    // ヘッダー（画像データのサイズ）を受信
+                    byte[] header = new byte[4];
+                    int headerBytesRead = stream.Read(header, 0, header.Length);
 
-        // 受け取った文字列に文字を付け足して戻す
-        Byte[] buffer = System.Text.Encoding.ASCII.GetBytes("responce: " + responseData);
-        Stream.Write(buffer, 0, buffer.Length);
+                    if (headerBytesRead == 0)
+                    {
+                        Debug.Log("Client disconnected");
+                        break; // クライアントが切断された場合、ループを抜ける
+                    }
+
+                    int dataSize = BitConverter.ToInt32(header, 0);
+                    Debug.Log($"Data size: {dataSize}");
+                    if (dataSize > 0)
+                    {
+                        // 画像データを受信
+                        byte[] imageData = new byte[dataSize];
+                        int totalRead = 0;
+
+                        while (totalRead < dataSize)
+                        {
+                            int bytesRead = stream.Read(imageData, totalRead, dataSize - totalRead);
+                            if (bytesRead == 0) break; // データがなくなったら終了
+                            totalRead += bytesRead;
+                            
+                        }
+
+                        // 画像データを処理（ここでは単純にログ出力）
+                        Debug.Log($"Received image data of size {imageData.Length}");
+                    }
+                
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Error handling client: {e.Message}");
+        }
+    }
+    private void DisplayImage(byte[] imageData)
+    {
+        // 画像データをTexture2Dに変換
+        texture = new Texture2D(2, 2); // 画像サイズは後で更新される
+        texture.LoadImage(imageData);  // 画像データをロード
+
+        // RawImageにテクスチャを設定
+        rawImage.texture = texture;
+        rawImage.SetNativeSize();  // 画像のサイズを設定
     }
 }
